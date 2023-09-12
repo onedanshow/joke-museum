b1c4fdcd50dc9efbb53a15179e40642440623fcf
Daniel Dixon <code@danieldixon.com>
2023-09-12 09:07:10 -0600

    Only match published pages.

--- app/services/match_pages.rb
+++ app/services/match_pages.rb
@@ -15,6 +15,6 @@ class MatchPages
     # Limit the page_ids to the first 50
     related_page_ids = related_page_ids.take(50)
     
-    Page.where(id: related_page_ids).pluck(:handle).uniq
+    Page.published.where(id: related_page_ids).pluck(:handle).uniq
   end
 end
